5767bb61dd79052a02ac78f01d53ddfa29052008
xwEscanor <xw87540871@163.com>
2019-07-25 15:41:09 +0800

    多页面应用

---
+++ src/mypages/page1/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <a href="../index/index.html">index</a>
+    <h3>page1HTML-page1VUE</h3>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
